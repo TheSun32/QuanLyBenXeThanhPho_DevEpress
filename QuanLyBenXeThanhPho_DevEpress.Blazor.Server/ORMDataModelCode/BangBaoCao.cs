ee3b750e6f5c4e7afecd0c6858014f2d84e3ba55
tuyn286 <nguyenhoangtuyen286@gmail.com>
2024-06-27 23:51:06 +0700

    Add project files.

---
+++ QuanLyBenXeThanhPho_DevEpress.Blazor.Server/ORMDataModelCode/BangBaoCao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base;
+namespace QuanLyBenXeThanhPho_DevEpress.Blazor.Server.ORMDataModel
+{
+    [DefaultClassOptions]
+    public partial class BangBaoCao
+    {
+        public BangBaoCao(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
